3bce020b96e73b3a2a15fa6a463bc816c6cef956
Joshua Graber <joshua.d.graber@gmail.com>
2023-10-11 11:05:57 -0400

    feat(cli): copyStyleAssets and copyAllAssets Add methods, add arguments to trigger

--- bin/pdap-design-system-cli.js
+++ bin/pdap-design-system-cli.js
@@ -6,12 +6,18 @@ const cli = require('../src/cli');
 const args = require('minimist')(process.argv);
 
 switch (true) {
+	case args['copy-assets']:
+		return cli.copyAllAssets(args);
 	case args['copy-images']:
 		return cli.copyImageAssets(args);
+	case args['copy-styles']:
+		return cli.copyStyleAssets(args);
 	default:
 		return console.log(
 			'No option argument was passed to pdap-design-system CLI.',
 			'\n Current options are:',
-			'\n --copy-images: Copy image assets to assets/ (default) or custom path passed to optional --to argument.'
+			'\n --copy-assets: Copy all assets to assets/ (default) or custom path passed to optional --to argument.',
+			'\n --copy-images: Copy only image assets to assets/ (default) or custom path passed to optional --to argument.',
+			'\n --copy-styles: Copy only CSS assets to assets/ (default) or custom path passed to optional --to argument.'
 		);
 }
