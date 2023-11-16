224b9683fbf55af51c9dfaef71d3f2018a1accb5
Joshua Graber <joshua.d.graber@gmail.com>
2023-11-16 09:50:06 -0500

    update(Input): remove comments

--- src/components/Input/types.ts
+++ src/components/Input/types.ts
@@ -1,15 +1,7 @@
-// import * as vuelidateValidators from '@vuelidate/validators';
-// import { Validation } from '@vuelidate/core';
-// import { PdapFormData } from '../Form/PdapForm.vue';
-// import { Ref } from 'vue';
-
 export enum PdapInputTypes {
 	CHECKBOX = 'checkbox',
 	TEXT = 'text',
 }
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-// export type VuelidateInstance = Ref<Validation<{}, Record<string, unknown>>>;
 
 export interface PdapInputBaseProps {
 	errorMessage: string | undefined | null;
