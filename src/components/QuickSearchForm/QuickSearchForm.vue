39b28b1a269e7e4500b8616178c8515bc39f3956
Joshua Graber <joshua.d.graber@gmail.com>
2023-12-07 11:03:53 -0500

    chore(cleanup): remove logs

--- src/components/QuickSearchForm/QuickSearchForm.vue
+++ src/components/QuickSearchForm/QuickSearchForm.vue
@@ -53,7 +53,6 @@ const formSchema = [
 ];
 
 function handleSubmit(values: { location: string; searchTerm: string }) {
-	console.log({ values });
 	let { location, searchTerm } = values;
 	location = location && location.length > 0 ? location : 'all';
 	searchTerm = searchTerm && searchTerm.length > 0 ? searchTerm : 'all';
