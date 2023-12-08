9bdde2ead1cbbf7c38c288b11448daee990a6584
Josh Chamberlain <josh@getwhiplash.com>
2023-12-08 11:40:33 -0500

    refactor: button indecisiveness

--- src/components/Button/PdapButton.vue
+++ src/components/Button/PdapButton.vue
@@ -30,7 +30,7 @@ const classes = reactive({
 
 @layer components {
 	.pdap-button {
-		@apply cursor-pointer border-2 border-brand-gold decoration-0 disabled:opacity-50 font-semibold inline-block mx-1 px-6 py-3 rounded-none text-center text-lg w-full;
+		@apply cursor-pointer border-2 border-brand-gold decoration-0 disabled:opacity-50 font-semibold inline-block mx-1 px-6 py-2 rounded-none text-center text-lg w-full;
 		@apply hover:brightness-85 lg:text-xl sm:max-w-max;
 	}
 
