d5d0b060f5e03eac3b6096abc7e0378afad1a190
Joshua Graber <joshua.d.graber@gmail.com>
2023-12-18 09:11:26 -0500

    fix: update build badge script

--- tools/generateBuildBadge.js
+++ tools/generateBuildBadge.js
@@ -1,45 +1,37 @@
 #!/usr/bin/env node
+/* eslint-disable no-undef */
 import fs from 'fs';
 import path from 'path';
 import url from 'url';
 import { makeBadge } from 'badge-maker';
 import { exec } from 'child_process';
-import process from 'process';
 
 // Shim
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-async function writeBuildBadge() {
-	let isError = false;
+let isError = false;
 
-	await exec('npm run build', (error) => {
-		if (error) {
-			isError = true;
-		}
-	});
+exec('npm run build', (error) => {
+	if (error) {
+		isError = true;
+	}
+});
 
-	fs.writeFile(
-		path.join(__dirname, '../badges/build.svg'),
-		makeBadge({
-			label: 'build',
-			message: isError ? 'failing' : 'passing',
-			color: isError ? 'red' : 'brightgreen',
-			style: 'for-the-badge',
-		}),
-		(err) => {
-			if (err) console.log(err);
-			else {
-				console.log('Build badge written successfully with contents:\n');
-				console.log(fs.readFileSync('badges/build.svg', 'utf8'));
-			}
-		}
-	);
-}
+const badge = makeBadge({
+	label: 'build',
+	message: isError ? 'failing' : 'passing',
+	color: isError ? 'red' : 'brightgreen',
+	style: 'for-the-badge',
+});
 
-try {
-	await writeBuildBadge();
-	process.exit(0);
-} catch (e) {
-	console.error(e);
-	process.exit(1);
-}
+fs.writeFile(path.join(__dirname, '../badges/build.svg'), badge, (err) => {
+	if (err) console.log(err);
+	else {
+		console.log('Build badge written successfully with contents:\n');
+		console.log(fs.readFileSync('badges/build.svg', 'utf8'));
+	}
+
+	if (typeof fs.readFileSync('badges/build.svg', 'utf8') !== 'undefined') {
+		process.exit(0);
+	}
+});
