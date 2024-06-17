3becf58611ef4198ad045776de2529877f810690
Joshua Graber <joshua.d.graber@gmail.com>
2024-06-17 12:59:37 -0400

    chore(config): update linting configs

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,5 +1,14 @@
+/**  
+ * 
+ * NOTE: this is not imported from `/config`, so any updates will have to be made there as well for the time being (CJS / MJS mis-matches)
+ * TODO: figure out how to do this. (Maybe just a script?)
+ * 
+*/
 export default {
-	extends: ['stylelint-config-standard'],
+	extends: [
+		'stylelint-config-standard', 
+		"@ronilaukkarinen/stylelint-a11y/recommended"
+	],
 	rules: {
 		'at-rule-no-unknown': [
 			true,
@@ -16,6 +25,12 @@ export default {
 		'comment-empty-line-before': null,
 		'declaration-block-trailing-semicolon': null,
 		'no-descending-specificity': null,
+		"a11y/no-text-align-justify": true,
+		"a11y/font-size-is-readable": true,
+		"a11y/media-prefers-color-scheme": true,
+		"a11y/line-height-is-vertical-rhythmed": true,
+		"a11y/no-display-none": true,
+		"a11y/no-obsolete-element": true,
 	},
 	"overrides": [
     {
@@ -25,5 +40,4 @@ export default {
       ]
     }
   ]
-
 };
