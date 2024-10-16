b2673c9613053273c1352175d5fa3637f021d585
Joshua Graber <68428039+joshuagraber@users.noreply.github.com>
2024-10-15 20:10:44 -0400

    fix(components): export textarea component (#112)

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,10 @@ export * from './components/Footer/types';
 export * from './components/Form/types';
 export * from './components/FormV2/types';
 export * from './components/Header/types';
-export * from './components/Input/types';
+// export * from './components/Input/types';
+export * from './components/InputCheckbox/types';
+export * from './components/InputText/types';
+export * from './components/InputTextArea/types';
 export * from './components/Nav/types';
 export * from './components/TileIcon/types';
 
