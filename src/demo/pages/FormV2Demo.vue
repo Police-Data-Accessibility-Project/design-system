5aad8e80f4746a1685a6acf46e23ab6da369a6b6
Joshua Graber <joshua.d.graber@gmail.com>
2024-11-12 15:49:06 -0500

    fix: button children again

--- src/demo/pages/FormV2Demo.vue
+++ src/demo/pages/FormV2Demo.vue
@@ -60,7 +60,7 @@
 				:rows="5"
 			/>
 
-			<Button type="submit">Submit</Button>
+			<Button :is-loading="mockLoading" type="submit">Submit</Button>
 		</FormV2>
 	</main>
 </template>
@@ -73,6 +73,15 @@ import { InputCheckbox } from '../../components/InputCheckbox';
 import { InputPassword } from '../../components/InputPassword';
 import { InputSelect } from '../../components/InputSelect';
 import PdapInputTextArea from '../../components/InputTextArea/PdapInputTextArea.vue';
+import { onMounted, ref } from 'vue';
+
+const mockLoading = ref(true);
+
+onMounted(() => {
+	setTimeout(() => {
+		mockLoading.value = false;
+	}, 5000);
+});
 
 const INPUT_CHECKBOX_NAME = 'ice-cream';
 const INPUT_TEXT_PLACEHOLDER = 'Paul';
